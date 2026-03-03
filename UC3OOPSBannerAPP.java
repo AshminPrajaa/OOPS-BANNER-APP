74ad20a61e7f18a3a7835e5572e5312df95bbfb3
AshminPrajaa <ashminprajaa27@gmail.com>
2026-03-03 15:01:51 +0530

    use case 3 print oops

---
+++ UC3OOPSBannerAPP.java
@@ -0,0 +1,12 @@
+public class UC3OOPSBannerAPP {
+   public static void main(String[] var0) {
+      System.out.println(String.join(" ", " ***** ", " ***** ", " ****** ", " ***** "));
+      System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     *"));
+      System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*      "));
+      System.out.println(String.join(" ", "*     *", "*     *", " ****** ", " ***** "));
+      System.out.println(String.join(" ", "*     *", "*     *", "*       ", "      *"));
+      System.out.println(String.join(" ", "*     *", "*     *", "*       ", "*     *"));
+      System.out.println(String.join(" ", " ***** ", " ***** ", "*       ", " ***** "));
+   }
+
+}
