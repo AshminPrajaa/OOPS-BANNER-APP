3b0c0a4191522c3bd157a79480bab9a362dc46b6
AshminPrajaa <ashminprajaa27@gmail.com>
2026-03-12 09:54:28 +0530

    uc5 is soln

---
+++ UC5OOPSBannerAPP.java
@@ -0,0 +1,11 @@
+public class UC5OOPSBannerAPP {
+    public static void main(String[] args){
+        String.join("","    ***       ","   *****  ");
+        String.join(""," **     **    "," **       ");
+        String.join("","**       **   ","**        ");
+        String.join("","**       **   "," *****    ");
+        String.join("","**       **   ","      **  ");
+        String.join(""," **      **   ","**    **  ");
+        String.join("","     ***      ","  *****   ");
+    }
+}
